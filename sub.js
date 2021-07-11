d6e6f4bf1d3c8ea6eb37a00d5162f4e6c460304e
shivamkumar91 <shivam.kumar@tothenew.com>
2021-07-11 17:54:53 +0530

    commit after changing files

--- sub.js
+++ sub.js
@@ -4,17 +4,14 @@ if (Hls.isSupported()) {
 	hls.attachMedia(video);
 	hls.loadSource('https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8');
 	hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-		console.log(
-		  'manifest loaded, found ' + data.levels.length + ' quality level'
-		);
-		console.log(data.subtitleTracks + "  -----------------");
 		for(var i=0; i<data.subtitleTracks.length; i++){
 			console.log(data.subtitleTracks[i]);
 		}
 	});
 	
 	function setSub(){
-		if(document.getElementById('eng_sub').checked){
+		var checkedElement = document.getElementById('eng_sub');
+		if(checkedElement.checked){
 			hls.subtitleTrack = 0;
 		}else{
 			hls.subtitleTrack = 1;
@@ -25,6 +22,7 @@ if (Hls.isSupported()) {
 		console.log(
 		  'manifest loaded, found ' + data.levels.length + ' quality level'
 		);
+		// For Default Subtitle
 		hls.subtitleTrack = 0;
 		hls.subtitleDisplay = true;
 		video.play();
